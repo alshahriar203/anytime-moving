7bad143e80d8da55cf1a7067bd3fee9f5eef95c5
Mahir Al Shahriar <mahir@Mahirs-MacBook-Air.local>
2024-10-10 23:16:04 +0600

    fixed some issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import '../styles/common.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/FooterFinal';
+// import styles from './page.module.scss';
 
+// import cn from 'classnames';
 export const metadata = {
   title: 'Code Machines Academy - Fun & Engaging Coding for Kids',
   description: 'Code Machines is a growing academy and educational services provider for STEAM subjects in Halifax, Nova Scotia. Our mission is to equip kids with the tools they need to thrive in an evolving technological world through practical approaches',
--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,8 @@ import cn from 'classnames';
 import styles from './page.module.scss'; // Landing page-specific styles
 import '../styles/common.css';
 import '../styles/fonts.css';
+// import FooterFinal from '../components/FooterFinal';
+// import Navbar from '../components/Navbar';
 
 
 export default function LandingPage(props) {
